f3cdcbf3c8811a7725c26cba9b58e164dc9abff6
Irina Stakhiyevich <aleksandrovich.ira@gmail.com>
2017-09-11 17:07:22 +0300

    Setting up the environment

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    context: path.join(__dirname, 'src'),
+
+    entry: {
+        home: './App'
+    },
+
+    output: {
+        path: path.join(__dirname, "built"),
+        filename: '[name].js'
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    plugins: ["transform-react-jsx"],
+                    presets: ['env']
+                }
+            }
+        }]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Test',
+            hash: true,
+            template: './index.html'
+        }),
+    ],
+};
